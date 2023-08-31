dd1022902a39366df05718b5eb32e0af8ba7ebb5
Aerh <22944369+Aerhhh@users.noreply.github.com>
2023-08-31 02:57:10 +0100

    Add argument support to /gameinfo

--- src/main/java/net/aerh/skywars/command/GameInfoCommand.java
+++ src/main/java/net/aerh/skywars/command/GameInfoCommand.java
@@ -18,7 +18,7 @@ public class GameInfoCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (!(commandSender instanceof Player)) {
             commandSender.sendMessage("Only players can execute this command!");
             return true;
@@ -27,23 +27,33 @@ public class GameInfoCommand implements CommandExecutor {
         Player player = (Player) commandSender;
         SkyWarsGame game = plugin.findGame(player);
 
+        if (args.length > 0) {
+            game = plugin.getGame(args[0]);
+        }
+
         if (game == null) {
-            player.sendMessage(ChatColor.RED + "You are not in a game!");
+            if (args.length > 0) {
+                player.sendMessage(ChatColor.RED + "That world doesn't seem to hold a game!");
+            } else {
+                player.sendMessage(ChatColor.RED + "You are not in a game!");
+            }
             return true;
         }
 
-        String stringBuilder = ChatColor.GOLD + "Game Info (" + game.getWorld().getName() + "):" + "\n" +
-            ChatColor.YELLOW + "  State: " + ChatColor.RESET + game.getState() + "\n" +
-            ChatColor.YELLOW + "  Players: " + ChatColor.RESET + game.getBukkitPlayers().size() + "/" + SkyWarsGame.MAX_PLAYER_COUNT + "\n" +
-            ChatColor.YELLOW + "  Spectators: " + ChatColor.RESET + game.getBukkitSpectators().size() + "\n" +
-            ChatColor.YELLOW + "  Events: " + ChatColor.RESET + game.getGameLoop().getGameEvents().size() + "\n" +
-            ChatColor.YELLOW + "  Next Event: " + ChatColor.RESET + "In " + game.getGameLoop().getTimeUntilNextEvent() +
-            ChatColor.GRAY + " (" + game.getGameLoop().getNextEvent().getClass().getSimpleName() + ")" + "\n" +
-            ChatColor.YELLOW + "  Winner: " + ChatColor.RESET + (game.getWinner() != null ? game.getWinner().getUuid() : "None") + "\n" +
-            ChatColor.YELLOW + "  Islands: " + ChatColor.RESET + game.getIslands().size() + "\n" +
-            ChatColor.YELLOW + "  Chests: " + ChatColor.RESET + game.getRefillableChests().size() + "\n";
+        StringBuilder stringBuilder = new StringBuilder();
 
-        player.sendMessage(stringBuilder);
+        stringBuilder.append(ChatColor.GOLD + "Game Info (").append(game.getWorld().getName()).append("):").append("\n")
+            .append(ChatColor.YELLOW).append("  State: ").append(ChatColor.RESET).append(game.getState()).append("\n")
+            .append(ChatColor.YELLOW).append("  Players: ").append(ChatColor.RESET).append(game.getBukkitPlayers().size()).append("/").append(SkyWarsGame.MAX_PLAYER_COUNT).append("\n")
+            .append(ChatColor.YELLOW).append("  Spectators: ").append(ChatColor.RESET).append(game.getBukkitSpectators().size()).append("\n")
+            .append(ChatColor.YELLOW).append("  Events Remaining: ").append(ChatColor.RESET).append(game.getGameLoop().getGameEvents().size()).append("\n")
+            .append(ChatColor.YELLOW).append("  Next Event: ").append(ChatColor.RESET).append("In ").append(game.getGameLoop().getTimeUntilNextEvent())
+            .append(ChatColor.GRAY).append(" (").append(game.getGameLoop().getNextEvent().getClass().getSimpleName()).append(")").append("\n")
+            .append(ChatColor.YELLOW).append("  Winner: ").append(ChatColor.RESET).append(game.getWinner() != null ? game.getWinner().getUuid() : "None").append("\n")
+            .append(ChatColor.YELLOW).append("  Islands: ").append(ChatColor.RESET).append(game.getIslands().size()).append("\n")
+            .append(ChatColor.YELLOW).append("  Chests: ").append(ChatColor.RESET).append(game.getRefillableChests().size()).append("\n");
+
+        player.sendMessage(stringBuilder.toString());
         return false;
     }
 }
